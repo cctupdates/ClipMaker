d4873fbfc5fca0db8b664943ce19c01b938c0ab6
VON-ColdLand <tanuldeshmukh123@gmail.com>
2021-11-17 21:40:25 +0530

    testing build 2 heroku

--- server.js
+++ server.js
@@ -9,5 +9,15 @@ app.use(express.json({ extended: false }))
 
 // Upload Endpoint
 app.use('/upload', require('./routes/file'))
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, () => console.log('Server Started...'))
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+app.listen(PORT, () => console.log('Server Started...'))
